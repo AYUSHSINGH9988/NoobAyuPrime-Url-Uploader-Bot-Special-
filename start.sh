4524f4a9eba155ae8b7c9ea95ae3e9419e2c7396
AYUSHSINGH9988 <ayushsingh7337@gmail.com>
2026-04-11 21:46:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# 1. Start Aria2c Daemon in Background
 echo "🚀 Starting Aria2c..."
 aria2c --enable-rpc --rpc-listen-port=6800 --daemon --allow-overwrite=true --seed-time=0
 
-# 2. Wait 3 seconds to ensure it's running
 sleep 3
 
-# 3. Start the Bot
 echo "🤖 Starting Bot..."
 python3 main.py
