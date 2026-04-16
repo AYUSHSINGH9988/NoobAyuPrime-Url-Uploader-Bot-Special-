5fd98271d1cd8eb785934719129b643d61edc78e
AYUSHSINGH9988 <ayushsingh7337@gmail.com>
2026-04-16 14:08:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2424,5 +2424,7 @@ async def main():
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
 
